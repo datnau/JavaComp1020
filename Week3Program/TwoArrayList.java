9ae12667d9c13d3c46664ff16a17a6a1decb13e8
datnau <115642385+datnau@users.noreply.github.com>
2024-01-25 13:25:42 -0600

    dfgd

---
+++ Week3Program/TwoArrayList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TwoArrayList {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value for arr1: ");
+        String[] arr1 = new in.nextLine().split(" ");
+        ArrayList<String> arr1 = new ArrayList<String>();
+        System.out.println("Enter the value for arr2: ");
+        String[] arr2 = new in.nextLine().split(" ");
+        ArrayList<String> arr1 = new ArrayList<String>();
+
+    }
+}
